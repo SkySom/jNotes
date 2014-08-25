1bed4b7c5021cde203bc4527f3fd165f2216bbbd
SkySom <skylar.som@gmail.com>
2014-08-25 17:43:04 -0400

    Added application.conf.sample
    Also beginning of NoteController

---
+++ app/controllers/NoteController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+/**
+ * Created by Skylar on 8/25/2014.
+ */
+import play.api.mvc.{Action, Controller}
+
+object NoteController extends Controller {
+	def index = Action {
+		Ok();
+	}
+}
