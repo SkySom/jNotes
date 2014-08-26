cd60960fde7e1bedd4d0d8985adba841cd07c315
SkySom <skylar.som@gmail.com>
2014-08-26 14:50:56 -0400

    Fixed tabbing on User evolution

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,10 +3,11 @@
 # --- !Ups
 
 CREATE TABLE User (
-    userId bigint(20) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    PRIMARY KEY (userId ASC)
+  userId bigint(20) NOT NULL,
+  username varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY (userId ASC)
 );
 
 # --- !Downs
