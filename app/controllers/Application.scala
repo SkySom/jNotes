d7a1295206ee331ca2000cc25310830d5dcc5cc1
SkySom <skylar.som@gmail.com>
2014-09-02 02:44:47 -0400

    Fixed some formatting

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,9 +3,7 @@ package controllers
 import play.api.mvc._
 
 object Application extends Controller {
-
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
+	def index = Action {
+		Ok(views.html.index("Your new application is ready."))
+	}
 }
