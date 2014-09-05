ce263671341d5af4bc7fcdb5bfdeb6ba7b460b89
SkySom <skylar.som@gmail.com>
2014-09-04 22:10:13 -0400

    Added beginning of Registration method.

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -4,14 +4,25 @@ package controllers
  * Created by Skylar on 8/25/2014.
  */
 
+import play.api.libs.json.{JsValue, JsObject}
 import play.api.mvc.{Action, Controller}
+import play.api.libs.json._
 import models.{Note, User}
 
 import scala.util.{Failure, Success}
 
 object UserController extends Controller {
 	def create = Action(parse.json) { request =>
-		println(request.body.toString())
+		val userJson: JsValue = request.body.as[JsValue]
+		val username = userJson.\("username")
+		val email = userJson.\("email")
+		val password = userJson.\("password")
+		if(userJson.\("confirmpassword") != password) {
+			Ok(Json.obj(
+				"success" -> "false",
+				"message" -> "passwords must match"
+			))
+		}
 		Ok("")
 	}
 
