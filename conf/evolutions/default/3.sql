87fe65e79999481a030577df396f465f4aeebe5d
SkySom <skylar.som@gmail.com>
2014-09-10 12:38:53 -0400

    Deleted Everything.
    Moving to Java. Using Gradle as build, and then Spring and Hibernate

--- conf/evolutions/default/3.sql
+++
@@ -1,9 +0,0 @@
-#Add hash to users
-
-# --- !Ups
-ALTER table users
-  ADD COLUMN hash character varying(255) NOT NULL;
-
-# --- !Downs
-ALTER table users
-  DROP COLUMN hash;
--- conf/evolutions/default/4.sql
+++
@@ -1,7 +0,0 @@
-# --- !Ups
-ALTER table users
-  ADD CONSTRAINT adduniques UNIQUE (username, email);
-
-# --- !Downs
-ALTER table users
-  DROP CONSTRAINT adduniques;
