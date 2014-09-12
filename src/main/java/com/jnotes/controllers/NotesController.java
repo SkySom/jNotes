0d09aabbfada4247a5da73bd11365f61363d3b07
SkySom <skylar.som@gmail.com>
2014-09-12 17:05:46 -0400

    Get beginning of Spring Boot project.

---
+++ src/main/java/com/jnotes/controllers/NotesController.java
@@ -0,0 +1,18 @@
+package com.jnotes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Skylar on 9/12/2014.
+ */
+@Controller
+public class NotesController {
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
