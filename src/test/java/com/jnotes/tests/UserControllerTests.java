f9268f8b34e80a253b79f46fb409b06f78129a0e
SkySom <skylar.som@gmail.com>
2014-12-30 23:40:50 -0500

    Added Mockito
    Playing around with Mockito. Starting to see why I don't test much :/

--- src/test/java/com/jnotes/tests/UserControllerTests.java
+++ src/test/java/com/jnotes/tests/UserControllerTests.java
@@ -1,21 +1,33 @@
 package com.jnotes.tests;
 
+import com.jnotes.controllers.UserController;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 /**
  * Created by Skylar on 12/25/2014.
  */
 public class UserControllerTests {
+	MockMvc mockMvc;
+	UserController userController;
 
 	@Before
 	public void setup() {
+		userController = new UserController();
+		mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
 
 	}
-
 
 	@Test
 	public void CreateNewUser() {
 
+	}
+
+	@Test
+	public void GetNotExistentUser() throws Exception {
+		//mockMvc.perform(get("/users/{userId}", 0))
+				//.andExpect(status().isNotFound());
 	}
 }
